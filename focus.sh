b12dffa19e876a0a0738e63bbc4f1ebdfedd7c3d
niko <onescyt@gmail.com>
2022-05-23 05:06:27 +1000

    added readme

--- focus.sh
+++ focus.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 
-# play lofi radio if -m passed in
-while getopts "m" o; do
-    case "${o}" in
-        m)
-            echo -e "Loading lo-fi radio"
-            open https://www.youtube.com/watch?v=5qap5aO4i9A
-            sleep 5
-    esac
-done
-
-
 # append blocked websites to /etc/hosts
-cat blocked.txt | sudo tee -a /etc/hosts > /dev/null
+cat "$(dirname -- "$0")/blocked.txt" | sudo tee -a /etc/hosts > /dev/null
 
 # clear the dns cache 
 sudo dscacheutil -flushcache
@@ -25,12 +14,12 @@ SECONDS_LEFT=$(( ${1:-25} * 60 ))
 while (( $SECONDS_LEFT >= 0 ))
 do
     # capture ctrl-c
-    trap 'bash ./unfocus.sh; exit 0' INT
+    trap 'bash "$(dirname -- "$0")/unfocus.sh"; exit 0' INT
     echo -ne "\033[K"
     echo -ne "\rFocus! Sites will unblock in ${SECONDS_LEFT} seconds..."
     sleep 1
     SECONDS_LEFT=$(( $SECONDS_LEFT - 1 ))
 done
 
-bash ./unfocus.sh
+bash "$(dirname -- "$0")/unfocus.sh
 
--- unfocus.sh
+++ unfocus.sh
@@ -1,4 +1,6 @@
-grep -Fvxf blocked.txt /etc/hosts | sudo tee /etc/hosts > /dev/null
+
+
+grep -Fvxf "$(dirname -- "$0")/blocked.txt" /etc/hosts | sudo tee /etc/hosts > /dev/null
 sudo dscacheutil -flushcache
 sudo ifconfig en0 down
 sudo ifconfig en0 up
